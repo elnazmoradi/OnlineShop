809f5dacf8b9f77dba6a9597ce24b9169c441983
elnazmoradi <moradielnazr12@gmail.com>
2023-07-07 23:41:20 +0330

    Register API Added

---
+++ Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Service
+{
+  public class UserService : IUserService
+  {
+    private readonly IValidation validation;
+    private readonly IUserRepository userRepository;
+    public UserService(IValidation validation, IUserRepository userRepository)
+    {
+      this.validation = validation;   
+      this.userRepository = userRepository;
+    }
+    public string InsertUserInfo(User user)
+    {
+      if (!validation.NameValidation(user.FirstName))
+        return "Invalid First Name";
+
+      if (!validation.NameValidation(user.LastName))
+        return "Invalid Last Name";
+
+      if (!validation.EmailValidation(user.Email))
+        return "Invalid Email Address";
+
+      if (!validation.PhoneNumberValidation(user.PhoneNumber))
+        return "Invalid Phone Number";
+
+      if (!validation.UsernameValidation(user.UserName))
+        return "Invalid UserName";
+
+      if (!validation.PasswordValidation(user.Password))
+        return "Invalid Password";
+      
+      userRepository.InsertUserInfo(user);
+
+      return "Registered";
+    }
+  }
+}
---
+++ UI/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class UserController : ControllerBase
+  {
+    private readonly IUserService userService;
+    public UserController(IUserService userService)
+    {
+      this.userService = userService;
+    }
+    [HttpPost]
+    [ActionName("Register")]
+    [Route("/Register")]
+    public IActionResult Register(User user)
+    {
+      var result = userService.InsertUserInfo(user);
+      return Ok(result);
+    }
+  }
+}
