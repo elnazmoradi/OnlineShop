405c62919f5ad62e27ede0837ddc51b2038d37a5
elnazmoradi <moradielnazr12@gmail.com>
2023-07-08 21:06:15 +0330

    Revert "first stable version"
    This reverts commit c1640b123869243a93998422760b2c6a939e278f.

---
+++ DataAccess/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+  public static class DbContext
+  {
+    private static readonly string connectionString = @"Server=localhost;Database=OnlineShop;Trusted_Connection=True;TrustServerCertificate=True;";
+    public static SqlConnection Connection() => new SqlConnection(connectionString);
+  }
+}
