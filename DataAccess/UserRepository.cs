b101a05b547be51e8fc965227c57076df83953b6
Enigma <a.doostmoradi@gmail.com>
2023-07-08 21:05:58 +0330

    log in first version

--- DataAccess/UserRepository.cs
+++ DataAccess/UserRepository.cs
@@ -10,32 +10,32 @@ namespace DataAccess
         public User getUserByUsername(string username)
         {
 
-                User? user = null;
+            User? user = null;
 
-                SqlConnection connection = DbContext.Connection();
-                using (connection)
-                {
-                    connection.Open();
-                    SqlCommand sqlCommand = new("select * from Users where UserName = @UserName", connection);
+            SqlConnection connection = DbContext.Connection();
+            using (connection)
+            {
+                connection.Open();
+                SqlCommand sqlCommand = new("select * from Users where UserName = @UserName", connection);
                 SqlParameter sqlParameter = new()
                 {
                     ParameterName = "UserName",
                     Value = user.UserName
-                    };
-                    sqlCommand.Parameters.Add(sqlParameter);
+                };
+                sqlCommand.Parameters.Add(sqlParameter);
 
-                    using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                {
+                    if (reader.Read())
                     {
-                        if (reader.Read())
-                        {
-                            user = new(id: reader.GetGuid(0), firstName: reader.GetString(1), lastName: reader.GetString(2).Trim(), phoneNumber: reader.GetString(3).Trim(),
-                        address: reader.GetString(3), userName: reader.GetString(4).Trim(), password: reader.GetString(5), balance: reader.GetDecimal(6));
-                        }
-
-                        return user ?? null;
+                        user = new(id: reader.GetGuid(0), firstName: reader.GetString(1), lastName: reader.GetString(2).Trim(), phoneNumber: reader.GetString(3).Trim(),
+                    address: reader.GetString(3), userName: reader.GetString(4).Trim(), password: reader.GetString(5), balance: reader.GetDecimal(6));
                     }
 
-                }         
+                    return user ?? null;
+                }
+
+            }
         }
 
         public void InsertUserInfo(User user)
@@ -46,18 +46,19 @@ namespace DataAccess
                 string query = $@"INSERT INTO Account(FirstName,
                                            LastName,
                                            PhoneNumber,
-                                           Email,
+                                           Address,
                                            UserName,
-                                           Password) 
+                                           Password,
+                                           Balance) 
                                     VALUES(
                                             '{user.FirstName}',
                                             '{user.LastName}',
                                             '{user.PhoneNumber}',
                                             '{user.Address}',
                                             '{user.UserName}',
-                                            HASHBYTES('SHA2_256','{user.Password}')  
+                                            HASHBYTES('SHA2_256','{user.Password}'),
+                                            '{user.Balance}'
                                             )";
-
                 connection.Query(query);
             }
         }
