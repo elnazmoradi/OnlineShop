48c9e5d0f2c5f8e2797a4788fb15d07671ffebf4
salycode80 <salycode@gmail.com>
2023-07-08 23:31:40 +0330

    complete DAL Oerder

--- DataAccess/OrderRepository.cs
+++ DataAccess/OrderRepository.cs
@@ -23,6 +23,7 @@ namespace DataAccess
                 {
                     new SqlParameter("@ID",order.ID),
                     new SqlParameter("@SocksID",order.SocksID),
+                    new SqlParameter("@CartID",order.CartID),
                     new SqlParameter("@SocksNumber",order.SocksNumber),
                     new SqlParameter("@OrderPrice",order.OrderPrice),
                 });
@@ -31,19 +32,65 @@ namespace DataAccess
             }
         }
 
-        public int DeleteOrder(int id)
+        public int DeleteOrder(string id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = DbContext.Connection())
+            {
+                string command = $"DELETE FROM Orders WHERE ID = {id};";
+                using var cmd = new SqlCommand(command, connection);
+                var result = cmd.ExecuteNonQuery();
+                return result;
+            }
+            
         }
 
         public List<Order> GetOrdersByCartID(string cartID)
         {
-            throw new NotImplementedException();
+            var orderList = new List<Order>();  
+            using(var connection = DbContext.Connection())
+            {
+                connection.Open();
+                var command = new SqlCommand($"SELECT * FROM Orders WHERE ID ={cartID}",connection);
+                using(var reader = command.ExecuteReader()) 
+                {
+                    while(reader.Read())
+                    {
+                        var order = new Order()
+                        {
+                            ID = reader.GetInt32(0),
+                            SocksID = reader.GetInt32(1),
+                            CartID = reader.GetInt32(2),
+                            SocksNumber= reader.GetInt32(3),
+                            OrderPrice = reader.GetDouble(4),
+                        };
+                        orderList.Add(order);
+                    }
+                }
+            }
+            return orderList;   
+            
         }
 
         public int UpdateOrder(Order order)
         {
-            throw new NotImplementedException();
+            using (var connection = DbContext.Connection())
+            {
+                connection.Open();
+                string commandString = $"UPDATE Orders SET ID=@ID , SocksID=@SocksID , CartID=@CatID , SocksNumber=@SocksNumber , OrderPrice=@OrderPrice" +
+                    $"WHERE ID = {order.ID}";
+                var command = new SqlCommand(commandString, connection);
+                command.Parameters.AddRange(new SqlParameter[]
+                {
+                    new SqlParameter("@ID",order.ID),
+                    new SqlParameter("@CartID",order.CartID),
+                    new SqlParameter("@SocksID",order.SocksID),
+                    new SqlParameter("@SocksNumber",order.SocksNumber),
+                    new SqlParameter("@OrderPrice",order.OrderPrice),
+                });
+                var result = command.ExecuteNonQuery();
+                return result;
+            }
+
         }
     }
 }
--- Domain/Interfaces/IOrderRepository.cs
+++ Domain/Interfaces/IOrderRepository.cs
@@ -11,7 +11,7 @@ namespace Domain.Contracts
     {
         public int AddOrder(Order order);
         public int UpdateOrder(Order order);
-        public int DeleteOrder(int id);
+        public int DeleteOrder(string id);
         public List<Order> GetOrdersByCartID(string cartID);
     }
 }
