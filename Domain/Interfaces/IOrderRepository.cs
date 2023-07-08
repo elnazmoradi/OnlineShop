4e31a484d635fdf12919d19d0e784d695e5b6c98
salycode80 <salycode@gmail.com>
2023-07-08 20:26:39 +0330

    Add IOrderRepository methods

--- Domain/Interfaces/IOrderRepository.cs
+++ Domain/Interfaces/IOrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,10 @@ namespace Domain.Contracts
 {
     public interface IOrderRepository
     {
+        public int AddOrder(Order order);
+        public int UpdateOrder(Order order);
+        public int DeleteOrder(int id);
+        public List<Order> GetOrdersByCartID();
     }
 }
 
